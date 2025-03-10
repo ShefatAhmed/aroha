5f0ef719fde6eedacabbce46f69f86c550e46f9b
unknown <shefata69@gmail.com>
2025-03-10 13:17:26 +0600

    section mergin

---
+++ src/pages/Home/Home.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+
+const Home = () => {
+  return <div>
+    <h1 className="font-extrabold text-9xl text-center">Carousel!</h1>
+    <h1 className="font-extrabold text-9xl text-center">Features Category!</h1>
+    <h1 className="font-extrabold text-9xl text-center">Discover the latest trends!</h1>
+    <h1 className="font-extrabold text-9xl text-center">Amazing deals, updated daily!</h1>
+    <h1 className="font-extrabold text-9xl text-center">Holiday Deals!</h1>
+    <h1 className="font-extrabold text-9xl text-center">Shop extraordinary!</h1>
+  </div>;
+};
+
+export default Home;
--- src/routes/routes.jsx
+++ src/routes/routes.jsx
@@ -1,17 +1,37 @@
 import { createBrowserRouter } from "react-router-dom";
 import App from "../App";
-import Home from "../pages/Home";
+import Home from "../pages/Home/Home";
+import CategoryDetails from "../pages/CategoryDetails/CategoryDetails";
+import ProductDetails from "../pages/ProductDetails/ProductDetails";
+import Cart from "../pages/Cart/Cart";
+import ShopDetails from "../pages/ShopDetails/ShopDetails";
 const router = createBrowserRouter([
-    {
-      path: "/",
-      element: <App></App>,
-      children: [
-        {
-          path: '/',
-          element: <Home></Home>
-        },
-      ]
-    },
-  ]);
+  {
+    path: "/",
+    element: <App />,
+    children: [
+      {
+        path: "/",
+        element: <Home />,
+      },
+      {
+        path: "/category-details",
+        element: <CategoryDetails />,
+      },
+      {
+        path: "/product-details",
+        element: <ProductDetails />,
+      },
+      {
+        path: "/shop-details",
+        element: <ShopDetails />,
+      },
+      {
+        path: "/cart-details",
+        element: <Cart />,
+      },
+    ],
+  },
+]);
 
 export default router;
